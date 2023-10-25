2b5acbd230e59e4725972ad904ee14ff84787523
Marik <letschuka@gmail.com>
2023-10-25 11:23:54 +0300

    final

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('user/edit/<int:user_id>/', views.user_edit_logic, name='user_edit_logic'),
     path('user/delete/<int:user_id>/', views.user_delete_logic, name='user_delete_logic'),
     path('group/list/', views.group_list_page, name='group_list_page'),
-    path('gropu/add/', views.group_add_logic, name='group_add_logic'),
+    path('group/add/', views.group_add_logic, name='group_add_logic'),
     path('group/edit/<int:group_id>/', views.group_edit_logic, name='group_edit_logic'),
     path('group/delete/<int:group_id>/', views.group_delete_logic, name='group_delete_logic'),
 ]
